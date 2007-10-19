a1c371433c0d7a47529201c825e29290f98c546b
Erik Hetzner <egh@e6h.org>
2007-10-19 15:14:53 -0700

    add genre class for Playlist, examples
    --HG--
    branch : daap_server.py.d
    extra : convert_revision : egh%40xam-20071019221453-kdnj8gap9hbdqf06

--- playlists.py
+++ playlists.py
@@ -1,7 +1,28 @@
 class Playlist:
+    name = None
     pass
 
 class Library(Playlist):
     name = "Library"
     def contains(self, md):
         return True
+
+class Genre(Playlist):
+    def contains(self, md):
+        h = md.get_md()
+        if not(h.has_key('daap.songgenre')): return False
+        else:
+            songgenre = h['daap.songgenre'].lower()
+            matchgenre = self.__class__.genre
+            if type(matchgenre) == str:
+                return songgenre == matchgenre
+            elif type(matchgenre) == list:
+                return songgenre in matchgenre
+
+#class Reggae(Genre):
+#    name = "reggae"
+#    genre = ["reggae", "reggae: dub", "dub"]
+
+#class Classical(Genre):
+#    name = "classical"
+#    genre = ["classical", "avant garde"]
