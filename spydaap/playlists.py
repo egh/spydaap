1dfae16758b083237f215b3cacee74ea284a9431
Erik Hetzner <egh@e6h.org>
2007-10-21 19:15:21 -0700

    added Builder
    --HG--
    extra : convert_revision : egh%40xam-20071022021521-xztfiwwfgarxz0n1

--- spydaap/playlists.py
+++ spydaap/playlists.py
@@ -1,3 +1,32 @@
+from spydaap.daap import do
+
+class Builder:
+    def build(self, f):
+        def build_do(md):
+            return do('dmap.listingitem',
+                      [ do('dmap.itemkind', 2),
+                        do('dmap.itemid', md.id + 1),
+                        do('dmap.itemname', md.get_name()),
+                        do('dmap.containeritemid', md.id + 1)
+                        ] )
+
+        for x in dir(self.__module__):
+            pclass = self.__module__.__dict__[x]
+            if type(pclass) == types.ClassType and \
+                    issubclass(pclass, Playlist) and \
+                    pclass.name != None:
+                pl = pclass()
+                entries = [n for n in self.metadata_cache if pl.contains(n)]
+                d = do('daap.playlistsongs',
+                       [ do('dmap.status', 200),
+                         do('dmap.updatetype', 0),
+                         do('dmap.specifiedtotalcount', len(entries)),
+                         do('dmap.returnedcount', len(entries)),
+                         do('dmap.listing',
+                            [ build_do (n) for n in entries ])
+                         ])
+                f.write(d.encode())
+
 class Playlist:
     name = None
     pass
