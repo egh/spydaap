c9e09338d2fe8a34b6d6d59860bf6b616a1ed64b
Erik Hetzner <egh@e6h.org>
2007-11-28 11:30:59 -0800

    make media path configurable
    --HG--
    extra : convert_revision : ehetzner%40gmail.com-20071128193059-ss8e38yg8ox22ftu

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import spydaap
-from  spydaap.parser import mp3, avi
+from spydaap.parser import mp3
+#to process .mov files
+#from spydaap.parser import mp3,mov
+
 #spydaap.server_name = "spydaap"
 #spydaap.port = 3689
+
+#top path to scan for media
+#spydaap.media_path = "media"
--- spydaap/__init__.py
+++ spydaap/__init__.py
@@ -1,2 +1,3 @@
 server_name = "spydaap"
 port = 3689
+media_path = "media"
