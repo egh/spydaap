5ab27984da16240b59de9a2a47f8fa08d3a6d44f
Erik Hetzner <egh@e6h.org>
2010-11-20 20:59:57 -0800

    add py_modules; try to use avahi, otherwise install pybonjour
    --HG--
    extra : convert_revision : ehetzner%40gmail.com-20101121045957-y7rijwy57ropmfdh

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 from setuptools import setup
+import platform
 
+reqs=["mutagen>=1.2"]
+# use avahi if available
+try:
+    import avahi
+except ImportError:
+    reqs.append("pybonjour>=1.1")
 
 setup(
     name="spydaap",
     version="0.1dev",
-    install_requires=[
-        'pybonjour>=1.1',
-        'mutagen>=1.2',
-    ],
+    install_requires=reqs,
     entry_points={
         'console_scripts': [
             'spydaap=spydaap.cli:main'
         ]
-    }
-)
+    },
+    py_modules=["spydaap.cache",
+                "spydaap.cli",
+                "spydaap.containers",
+                "spydaap.daap_data",
+                "spydaap.daap",
+                "spydaap.metadata",
+                "spydaap.playlists",
+                "spydaap.server",
+                "spydaap.zeroconf",
+                "spydaap.parser.avi",
+                "spydaap.parser.flac",
+                "spydaap.parser.mov",
+                "spydaap.parser.mp3",
+                "spydaap.parser.ogg",
+                "spydaap.parser.vorbis"])
