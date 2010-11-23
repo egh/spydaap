2fdc4dc8f40e60150b590981941bbf014304140b
Erik Hetzner <egh@e6h.org>
2010-11-22 23:38:59 -0800

    adding metadata to setup.py; use packages, not py_modules
    --HG--
    extra : convert_revision : ehetzner%40gmail.com-20101123073859-jt857huffitkg694

--- setup.py
+++ setup.py
@@ -11,24 +11,37 @@ except ImportError:
 setup(
     name="spydaap",
     version="0.1dev",
+    author="Erik Hetzner",
+    author_email="egh@e6h.org",
+    description="A simple DAAP server",
+    long_description="""
+=========
+ spydaap
+=========
+
+Spydaap is a media server supporting the DAAP protocol (aka iTunes
+sharing). It is written in Python, uses the mutagen media metadata
+library, and either the Avahi or pybonjour Zeroconf implementation.
+
+Features:
+
+ - Runs on Unix-like systems (Linux, *BSD, Mac OS X).
+ - Can stream mp3s, ogg, flac, and Quicktime videos.
+ - Supports "smart" playlists written in Python.
+ - Written in 100 percent Python and easily modifiable.
+ - Caches almost everything for fast performance.
+ - Embeddable.""",
+    url="http://launchpad.net/spydaap/",
     install_requires=reqs,
     entry_points={
         'console_scripts': [
             'spydaap=spydaap.cli:main'
         ]
     },
-    py_modules=["spydaap.cache",
-                "spydaap.cli",
-                "spydaap.containers",
-                "spydaap.daap_data",
-                "spydaap.daap",
-                "spydaap.metadata",
-                "spydaap.playlists",
-                "spydaap.server",
-                "spydaap.zeroconf",
-                "spydaap.parser.avi",
-                "spydaap.parser.flac",
-                "spydaap.parser.mov",
-                "spydaap.parser.mp3",
-                "spydaap.parser.ogg",
-                "spydaap.parser.vorbis"])
+    packages=["spydaap", "spydaap.parser"],
+    classifiers=["Development Status :: 4 - Beta",
+                 "License :: OSI Approved :: GNU General Public License (GPL)",
+                 "Programming Language :: Python",
+                 "Topic :: Multimedia :: Sound/Audio",
+                 "Operating System :: POSIX",
+                 "Intended Audience :: End Users/Desktop"])
